2e13f9fb3f705ced756ec343b1ba6d4183a6af73
Gustavo <gustavo.aroeira@gmail.com>
2022-07-07 23:26:40 -0400

    Clean up solution.c

--- 0_Programming/1_MonteCarlo/gustavo/solution.c
+++ 0_Programming/1_MonteCarlo/gustavo/solution.c
@@ -2,11 +2,44 @@
 #include <stdlib.h>
 #include <math.h>
 
+// Auxiliary function to compute random numbers from -1.0 to 1.0
 double get_rand() {
-    int x;
-    x = rand();
-    return (float) 2*x / (RAND_MAX) - 1.0;
+    return 2.0 * rand() / (RAND_MAX) - 1.0;
 }
+
+// This function takes in a number of samples and returns the estimate value of pi
+double get_pi(int N) {
+
+    // Number of points inside the circle
+    int hit;
+
+    // x and y coordinate
+    double x, y;
+
+    // Distance to the center
+    double dist;
+
+    for (int i = 1; i <= N; i++) {
+
+        // Get x,y coordinates
+        x = get_rand();
+        y = get_rand();
+
+        // Computes distance from origin
+        dist = sqrt(x*x + y*y);
+
+        // Check if the point lies within the circle
+        if (dist <= 1.0) {
+
+            // If yes, increase the hit counter
+            hit++;
+        }
+    }
+
+    // Return pi
+    return 4.0 * hit / N;
+}
+
 
 int main() {
 
@@ -17,35 +50,18 @@ int main() {
     // Declare variables
     // Number of samples
     int N;
-
-    // x and y coordinate
-    double x, y;
-
-    // Distance to the center
-    double dist;
-
     // Pi value
     double pi;
-    
-    // Number of points inside the circle
-    int hit;
 
+    // Read input from user
     printf("Enter number of samples: ");
     scanf("%d", &N);
 
-    for (int i = 1; i <= N; i++) {
-        x = get_rand();
-        y = get_rand();
-        dist = sqrt(x*x + y*y);
+    // Call function to compute pi
+    pi = get_pi(N);
 
-        if (dist <= 1.0) {
-            hit++;
-        }
-    }
-
-    pi = 4.0 * hit / N;
-
-    printf("Estimated value of pi: %f", pi);
+    // Print out value
+    printf("Estimated value of pi: %f\n", pi);
 
     return 0;
 }
