8b58eca17f440178b6d27e59f4d32fee4a45bbf2
Kaihong Sun <89555427+KSun21@users.noreply.github.com>
2022-07-28 12:29:58 -0400

    Update Sine Calculator In Python.py

--- 0_Programming/2_SinCalculator/Kaihong/Sine Calculator In Python.py
+++ 0_Programming/2_SinCalculator/Kaihong/Sine Calculator In Python.py
@@ -1,11 +1,11 @@
 import math\n",
-def get_sin(x, precision):\n",
-   a = 0\n",
-   b = precision\n",
-   c = 0\n",
-   for i in range(100):\n",
-       c = a\n",
-       a = a + (-1)**(i) * x**(2*i+1)/math.factorial(2*i+1)\n",
-        if abs(a - c) <= 10**(-b-2):\n",
-            print(round(a,b), i+1)\n",
-            break"
+def get_sin(x, precision):
+   a = 0
+   b = precision
+   c = 0
+   for i in range(100):
+       c = a
+       a = a + (-1)**(i) * x**(2*i+1)/math.factorial(2*i+1)
+        if abs(a - c) <= 10**(-b-2):
+            print(round(a,b), i+1)
+            break
